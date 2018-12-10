9c13e0898fa55a37a0393f6aae523b84f9e64983
NathanAugustin <augustin@lacroixrouge-brest.fr>
2018-12-10 16:25:40 +0100

    server à l'écoute

--- serveur.py
+++ serveur.py
@@ -1,10 +1,14 @@
 import socket
 
-UDP_IP = "10.160.108.12"
+UDP_IP = "10.160.108.16"
 UDP_PORT = 5005
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
 
 while True:
+
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print ("Message recu :", data.decode())
+    print ("Ptite Chienne au chomdu dit :", data.decode())
+
+    REPONSE=input("Entre ta reponse : ")
+    sock.sendto(REPONSE.encode(),addr)
