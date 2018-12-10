48717a2d1b5178646d5f0231813e6b6ebbf84019
AdamFaes <adam.faes99@gmail.com>
2018-12-10 16:17:15 +0100

    Client

--- client.py
+++ client.py
@@ -4,7 +4,12 @@ UDP_IP = "10.160.108.16"
 UDP_PORT = 5005
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
-MESSAGE=("Connecté !")
-sock.sendto(MESSAGE.encode(), (UDP_IP, UDP_PORT))
-data,addr = sock.recvfrom(1024)
-print("",data.decode()) 
+while True:
+    MESSAGE=input("Message : ")
+    sock.connect((UDP_IP, UDP_PORT))
+    sock.send(MESSAGE.encode())
+#data,addr = sock.recv(1024)
+##print(data.decode())
+
+    data, addr= sock.recvfrom(1024)
+    print("Message : ", data.decode())
