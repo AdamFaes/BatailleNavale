37a4a6813778c546c0ddb57fb36e2599dd9894d3
AdamFaes <adam.faes99@gmail.com>
2018-12-13 10:55:29 +0100

    Ajout de la demande du serveur vers le client pour la position du bateau

--- client.py
+++ client.py
@@ -4,12 +4,29 @@ UDP_IP = "10.160.108.16"
 UDP_PORT = 5005
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
-while True:
-    MESSAGE=input("Message : ")
-    sock.connect((UDP_IP, UDP_PORT))
-    sock.send(MESSAGE.encode())
+
+MESSAGE=("Connecté !")
+sock.connect((UDP_IP, UDP_PORT))
+sock.send(MESSAGE.encode())
 #data,addr = sock.recv(1024)
 ##print(data.decode())
 
-    data, addr= sock.recvfrom(1024)
-    print("Message : ", data.decode())
+data, addr= sock.recvfrom(1024)
+print("Message Serveur : ", data.decode())
+
+sens=input()
+sock.send(sens.encode())
+
+data, addr= sock.recvfrom(1024)
+print("Message Serveur : ", data.decode())
+
+lig=input()
+sock.send(lig.encode())
+
+data, addr= sock.recvfrom(1024)
+print("Message Serveur : ", data.decode())
+
+col=input()
+sock.send(col.encode())
+
+data, addr= sock.recvfrom(1024)
